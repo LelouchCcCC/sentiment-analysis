f41c9bbe96d8b64c8823eb6c190383e778ebbc45
Yuhan <605286079@qq.com>
2024-04-05 00:43:08 -0400

    update some constant

--- src/main/scala/utils/Constants.scala
+++ src/main/scala/utils/Constants.scala
@@ -4,4 +4,8 @@ object Constants {
   val STOP_WORDS = "NLTK_English_Stopwords_Corpus.txt"
   val TRAINING_SET_PATH = ""
   val TESTING_SET_PATH = ""
+  val sentiment140TrainingFilePath = ""
+  val naiveBayesModelPath = ""
+  val sentiment140TestingFilePath = ""
+  val modelAccuracyPath = ""
 }
