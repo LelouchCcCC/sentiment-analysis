5eb1617abe8a6a897e26f9bbd1af1250defe9149
Yuhan <605286079@qq.com>
2024-04-05 17:04:36 -0400

    route api
    we call api here

--- src/main/scala/dataApi/DataApi.scala
+++ src/main/scala/dataApi/DataApi.scala
@@ -1,5 +1,12 @@
 package dataApi
-
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 object DataApi {
+  val route: Route =
+    path("test") {
+      get {
+        complete("This is a test route in DataApi object.")
+      }
+    }
 
 }
