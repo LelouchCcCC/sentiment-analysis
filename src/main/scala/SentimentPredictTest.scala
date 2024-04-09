b0f5776d51038d0da3b780ff9655662ed5cdb7a3
ansonzzh <anson.z.zheng@outlook.com>
2024-04-09 12:49:45 -0400

    Compare the difference of the sentiment from hour to hour

--- src/main/scala/SentimentPredictTest.scala
+++ src/main/scala/SentimentPredictTest.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.mllib.classification.NaiveBayesModel
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.TimestampType
 import sentiment.mllib.MLlibSentimentAnalyzer.computeSentiment
 import utils.{Constants, StopwordsLoader}
 
@@ -25,14 +26,14 @@ object SentimentPredictTest extends App {
   // 加载模型
   val modelPath = Constants.naiveBayesModelPath
   val naiveBayesModel = NaiveBayesModel.load(sc, modelPath)
-//  println(airlineData.show(300))
+  //  println(airlineData.show(300))
   val filteredAirlineData = airlineData.filter(col("airline_sentiment").rlike("^(neutral|positive|negative)$"))
   val dropNAAirline = filteredAirlineData.na.drop()
-//  println(dropNAAirline.show(30))
+  //  println(dropNAAirline.show(30))
   val processText = udf((s: String) => computeSentiment(s, stopWordsList, naiveBayesModel))
   val modifiedDf = dropNAAirline.withColumn("sentimentComputed", processText(col("text")))
   modifiedDf.show(400)
-//  println(modifiedDf.printSchema())
+  //  println(modifiedDf.printSchema())
   // 创建一个新列，如果两列值相等，则该列值为1，否则为0
   val dfWithInt = modifiedDf.withColumn("sentiment", col("sentiment").cast("int"))
   val comparisonDf = dfWithInt.withColumn("is_equal", when(col("sentiment") === col("sentimentComputed"), 1).otherwise(0))
@@ -41,4 +42,29 @@ object SentimentPredictTest extends App {
   val accuracy = comparisonDf.agg(sum("is_equal").cast("double") / count("is_equal")).first().get(0).asInstanceOf[Double]
 
   println(s"Accuracy: $accuracy")
+
+  // 将tweet_created转换为日期时间类型
+  val dfWithTimestamp = comparisonDf.withColumn("tweet_timestamp", unix_timestamp(col("tweet_created"), "M/d/yy H:mm").cast(TimestampType))
+
+  // 提取小时
+  val dfWithHour = dfWithTimestamp.withColumn("hour", hour(col("tweet_timestamp")))
+
+  // 按小时分组并计算sentiment和sentimentComputed的平均值
+  val averageSentimentByHour = dfWithHour.groupBy("hour")
+    .agg(
+      round(avg("sentiment"), 2).alias("average_sentiment"),
+      round(avg("sentimentComputed"), 2).alias("average_sentiment_computed")
+    )
+    .orderBy("hour")
+
+  // 显示结果
+  averageSentimentByHour.show()
+
+  val resultDf = averageSentimentByHour
+    .withColumn("abs_difference", round(abs(col("average_sentiment") - col("average_sentiment_computed")), 2))
+    .withColumn("squared_difference", round(pow(col("average_sentiment") - col("average_sentiment_computed"), 2), 2))
+
+
+  // 显示结果，包括新增的差异度量列
+  resultDf.show()
 }
