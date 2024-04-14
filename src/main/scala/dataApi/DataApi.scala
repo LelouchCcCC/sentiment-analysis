e70e834cd4e781b829aae5771f3a523818f04925
ansonzzh <anson.z.zheng@outlook.com>
2024-04-14 16:28:23 -0400

    update

--- src/main/scala/dataApi/DataApi.scala
+++ src/main/scala/dataApi/DataApi.scala
@@ -3,10 +3,9 @@ package dataApi
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import dataProcessor.DataProcessor.read_new_csv
-import org.apache.spark.mllib.classification.NaiveBayesModel
 import org.apache.spark.sql.SparkSession
 import utils.SentimentAnalyzer.singleAnalyze
-import utils.{Constants, SentimentAnalyzer, StopwordsLoader}
+
 import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Paths}
 import java.time.LocalDateTime
@@ -29,33 +28,34 @@ class DataApi(spark: SparkSession) {
 
   val route2: Route = {
     path("oneSentiment") {
-            get
-            parameter("data"){ sentiment =>
-              println(sentiment)
-              val res = singleAnalyze(spark, sentiment).toString
-              val newLine = createNewCsvLine("src/main/resources/data/airline.csv", res, sentiment)
-              appendToCsv("src/main/resources/data/airline.csv", newLine)
-              complete(res)
-              // should respond with 这句话的好坏-1/0/1
-              // 还要处理一下时间相关的东西，更新一下new_csv
-            }
+      get
+      parameter("data") { sentiment =>
+        println(sentiment)
+        val res = singleAnalyze(spark, sentiment).toString
+        val newLine = createNewCsvLine("src/main/resources/data/airline.csv", res, sentiment)
+        appendToCsv("src/main/resources/data/airline.csv", newLine)
+        complete(res)
+        // should respond with 这句话的好坏-1/0/1
+        // 还要处理一下时间相关的东西，更新一下new_csv
+      }
 
 
-//      get {
-//        parameter("data") { sentimentData =>
-//          println(s"Received sentiment data: $sentimentData")
-//          // 这里调用您的情感分析逻辑
-//          val res = analyzeSentiment(sentimentData)
-//
-//          // 处理CSV更新操作，如果需要的话
-//          updateCsvIfNeeded(spark)
-//
-//          // 根据分析结果返回响应
-//          complete(StatusCodes.OK, s"情感分析结果: $res")
-//        }
-//      }
+      //      get {
+      //        parameter("data") { sentimentData =>
+      //          println(s"Received sentiment data: $sentimentData")
+      //          // 这里调用您的情感分析逻辑
+      //          val res = analyzeSentiment(sentimentData)
+      //
+      //          // 处理CSV更新操作，如果需要的话
+      //          updateCsvIfNeeded(spark)
+      //
+      //          // 根据分析结果返回响应
+      //          complete(StatusCodes.OK, s"情感分析结果: $res")
+      //        }
+      //      }
     }
   }
+
   def getLastLine(filePath: String): String = {
     val lines = Files.readAllLines(Paths.get(filePath)).asScala
     lines.last
