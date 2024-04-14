5136897feb4d731c23437f6f2800dded510bd856
ansonzzh <anson.z.zheng@outlook.com>
2024-04-14 16:44:18 -0400

    update date Format

--- src/main/scala/dataApi/DataApi.scala
+++ src/main/scala/dataApi/DataApi.scala
@@ -14,7 +14,7 @@ import scala.jdk.CollectionConverters._
 
 
 class DataApi(spark: SparkSession) {
-  val dateTimeFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm")
+  val dateTimeFormat = DateTimeFormatter.ofPattern("M/d/yy HH:mm")
   //  val corsSettings = CorsSettings.defaultSettings.withAllowedOrigins(HttpOriginRange.*)
 
   val route: Route = {
