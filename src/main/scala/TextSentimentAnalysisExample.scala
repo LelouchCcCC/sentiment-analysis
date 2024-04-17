8a93886ad8ff7edd191e5b9f0ac9158b1cc3d0c9
Celestialfoxx <151230788+Celestialfoxx@users.noreply.github.com>
2024-04-17 15:45:00 -0400

    modify

--- src/main/scala/TextSentimentAnalysisExample.scala
+++ src/main/scala/TextSentimentAnalysisExample.scala
@@ -14,27 +14,26 @@ object TextSentimentAnalysisExample {
 
     val sc = spark.sparkContext
 
-    // 用于广播停用词列表
+    // load stop words
     val stopWordsList = sc.broadcast(StopwordsLoader.loadStopWords(Constants.STOP_WORDS))
 
-    // 加载模型
+    // load model
     val modelPath = Constants.naiveBayesModelPath
     val naiveBayesModel = NaiveBayesModel.load(sc, modelPath)
 
-    // 加载CSV文件
+    // load csv
     val filePath = s"../resources/data/${Constants.TESTING_CSV_FILE_NAME}"
     val df = loadData(spark,filePath)
 
-    // 定义一个UDF，用于调用MLlibSentimentAnalyzer进行情感分析
+    // use udf to compute sentiment value
     val sentimentAnalysisUdf = udf { text: String =>
       MLlibSentimentAnalyzer.computeSentiment(text, stopWordsList, naiveBayesModel)
     }
 
-    // 应用UDF对DataFrame的'text'列进行情感分析
-    val sentimentColumnName = "text" // 确保这是包含文本数据的列名
+    val sentimentColumnName = "text"
     val analyzedDF = df.withColumn("sentiment", sentimentAnalysisUdf(col(sentimentColumnName)))
 
-    // 显示结果
+    // show result
     analyzedDF.show()
 
     // 保存结果，更改为实际路径
